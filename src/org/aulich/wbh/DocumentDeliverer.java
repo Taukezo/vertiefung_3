a0e75d471d7e665bf59c6b03e055da6b506ded72
Taukezo <gittau@aulich.org>
2023-05-24 17:02:03 +0200

    Update 2

---
+++ src/org/aulich/wbh/DocumentDeliverer.java
@@ -0,0 +1,6 @@
+package org.aulich.wbh;
+import java.io.File;
+
+public interface DocumentDeliverer {
+    public File getNext();
+}
---
+++ src/org/aulich/wbh/FileQue.java
@@ -0,0 +1,15 @@
+package org.aulich.wbh;
+
+import java.io.File;
+
+public class FileQue implements DocumentDeliverer{
+
+    public FileQue(File basePoint) {
+
+    }
+
+    @Override
+    public File getNext() {
+        return null;
+    }
+}
