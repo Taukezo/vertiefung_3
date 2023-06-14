1cf6c25f3e935b652dc23cfbfe5ea9cb7710357d
Taukezo <gittau@aulich.org>
2023-06-14 21:12:37 +0200

    Start to add comments

--- src/org/aulich/wbh/FileFiFoStack.java
+++ src/org/aulich/wbh/FileFiFoStack.java
@@ -2,14 +2,29 @@ package org.aulich.wbh;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
+/**
+ * The Class provides an easy access to a stack of relevant files (identified by file-extension)
+ * that are suitable for fulltext-indexing.
+ *
+ * @author Thomas Aulich
+ */
 public class FileFiFoStack {
     private static final String[] extensions = {"txt", "doc", "docx", "pdf"};
     Queue<File> fileQueue = new LinkedList<File>();
     Queue<File> dirQueue = new LinkedList<File>();
 
 
+    /**
+     * In the constructor the class needs the entry-point for the stack.
+     * In case of a file it just adds this file to the stack. In case of a directory all
+     * relevant files of this directory and subdirectories will be added to the stack.
+     *
+     * @param basePoint
+     * @throws FileNotFoundException
+     */
     public FileFiFoStack(File basePoint) throws FileNotFoundException {
         if (basePoint.exists() && basePoint.isDirectory()) {
             if (basePoint.isDirectory()) {
@@ -22,6 +37,12 @@ public class FileFiFoStack {
         }
     }
 
+    /**
+     * Method delivers the next relevant file in the stack. It will return null,
+     * if no more file is available.
+     *
+     * @return nextFile
+     */
     public File getNext() {
         File nextFile = fileQueue.poll();
         while (nextFile == null) {
